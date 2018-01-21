9d861b3e3d52723ef7b5c5c852c5889f516a5eec
Ohems <finnboy11@hotmail.com>
2018-01-21 18:55:16 +0200

    Add knex config

---
+++ backend/database.js
@@ -0,0 +1,28 @@
+const knex = require('')
+const envConfig = require('./config');
+
+const config = {
+  client: 'pg',
+  connection: envConfig.DATABASE_URL,
+  migrations: {
+    tableName: 'migrations',
+  },
+  pool: {
+    min: 0,
+    max: 7,
+  },
+};
+
+let client = null;
+
+function connect() {
+  if (client) return client;
+
+  client = knex(config);
+  return client;
+}
+
+module.exports = {
+  config,
+  connect,
+}
