39568ef4e8622dad63d287b4c071e69c3dc1706d
Ohems <finnboy11@hotmail.com>
2018-01-25 22:23:31 +0200

    Add request logger middleware

--- backend/config.js
+++ backend/config.js
@@ -4,6 +4,7 @@ module.exports = {
   PORT: parseInt(process.env.PORT, 10) || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   LOGGER_LEVEL: process.env.LOGGER_LEVEL || 'info',
+  DISABLE_REQUST_LOGGER: process.env.DISABLE_REQUST_LOGGER === 'true',
 
   DATABASE_URL: process.env.DATABASE_URL,
 };
--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,14 @@ const path = require('path');
 const express = require('express');
 const fileRouter = require('express-file-router');
 const logger = require('./logger')(__filename);
+const requestLoggerMiddleware =
+  require('./middlewares/request-logger-middleware');
 const errorMiddleware = require('./middlewares/error-middleware');
 const config = require('./config');
 
 var app = express();
+
+app.use(requestLoggerMiddleware);
  
 //Load all files in endpoints 
 app.use(fileRouter.load(path.join(__dirname, 'routes')));
